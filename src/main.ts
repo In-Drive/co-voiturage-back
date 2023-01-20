fd0b7b0dfc7cd86b13ee9797b0d2d94adb59e644
dali012 <dali.jerbi97@gmail.com>
2023-01-21 00:53:11 +0100

    build: implements app middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { middleware } from './app.middleware';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  middleware(app);
   await app.listen(3000);
 }
 bootstrap();
