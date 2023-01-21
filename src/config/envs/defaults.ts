2f2f3b6fd91054c0a2b0d3fe81da91837cd12147
dali012 <dali.jerbi97@gmail.com>
2023-01-21 02:37:11 +0100

    build: implements envs config

--- src/config/envs/defaults.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  db: {
-    // entities: [`${__dirname}/../../entity/**/*.{js,ts}`],
-    // subscribers: [`${__dirname}/../../subscriber/**/*.{js,ts}`],
-    // migrations: [`${__dirname}/../../migration/**/*.{js,ts}`],
-  },
-  jwtSecret: process.env.JWT_SECRET,
-  jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
-};
--- src/config/envs/production.ts
+++ src/config/envs/production.ts
@@ -9,18 +9,8 @@ export const config = {
         port: process.env.DB_PORT || 3306,
         username: process.env.DB_USER || 'username',
         password: process.env.DB_PASSWORD || 'password',
-        database: process.env.DB_NAME || 'dbname',
+        database: process.env.DB_NAME || 'prod',
       },
-      slaves: [
-        {
-          // fix if necessary
-          host: 'slaveHost',
-          port: 5432,
-          username: 'username',
-          password: 'password',
-          database: 'dbname',
-        },
-      ],
     },
     extra: {
       connectionLimit: 30,
